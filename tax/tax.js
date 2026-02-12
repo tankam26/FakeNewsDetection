564938ab390e8f0e3fe9296c830bf596f9219435
Tania <taniak8922@gmail.com>
2026-02-12 16:55:42 +0530

    Remove unrelated project files from repo

--- tax/tax.js
+++
@@ -1,4 +0,0 @@
-const FY=document.getElementById("fy");
-const income=document.getElementById("income");
-const age=document.getElementsByClassName("age");
-const regime=document.getElementsByClassName("regime");
